a39d3f6cd56583142909f43fbfb796236b9e6c14
parzol <kontakt@sildev.pl>
2019-01-18 20:19:45 +0100

    Add dynamic loaded content for the History block

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './assets/scss/app.scss';
 import $ from 'cash-dom';
 import 'es6-promise/auto';
 import 'isomorphic-fetch';
+import {Template} from "./template";
 
 export class App {
   initializeApp() {
@@ -17,6 +18,16 @@ export class App {
             const { name, avatar_url, html_url, login, bio } = body
             self.update_profile(name, avatar_url, html_url, login, bio)
           })
+          .then(() => {
+            fetch('https://api.github.com/users/' + userName.val() + '/events/public')
+              .then(response => !response.ok ? Promise.reject(response.statusText) : response.json())
+              .then(body => {
+                $('#user-timeline').empty()
+                body.forEach(event => {
+                    self.update_history(event)
+                })
+              })
+          })
           .catch((error) => {
             console.log(error)
           })
@@ -31,6 +42,14 @@ export class App {
     $('#profile-image').attr('src', avatar_url)
     $('#profile-url').attr('href', html_url).text(login)
     $('#profile-bio').text(bio || '(no information)')
+  }
+
+  update_history(event) {
+    const template = new Template()
+    const html_template = template.loader(event)
+    if(html_template) {
+      $('#user-timeline').append(html_template)
+    }
   }
 
   validation_field(userName) {
